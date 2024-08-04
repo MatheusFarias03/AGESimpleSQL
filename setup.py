38f1c5eb29696235bd071cb75a4cf3f1a00c03d3
Matheus Farias <matheusfarias519@gmail.com>
2024-08-04 17:25:55 -0300

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="age_simple_sql",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
     install_requires=[
         'psycopg2-binary'
